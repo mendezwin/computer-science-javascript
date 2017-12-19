1cf08ca70371cde3a5b304d451d4057843b9175c
Edwin Mendez <mendezwin@gmail.com>
2017-12-19 14:23:16 -0500

    quicksort added

---
+++ algorithms/5 - Quick Sort.js
@@ -0,0 +1,32 @@
+/*
+
+  Quicksort!
+  
+  Name your function quickSort.
+  
+  Quicksort should grab a pivot from the end and then separate the list (not including the pivot)
+  into two lists, smaller than the pivot and larger than the pivot. Call quickSort on both of those
+  lists independently. Once those two lists come back sorted, concatenate the "left" (or smaller numbers)
+  list, the pivot, and the "right" (or larger numbers) list and return that. The base case is when quickSort
+  is called on a list with length less-than-or-equal-to 1. In the base case, just return the array given.
+
+
+*/
+
+const quickSort = nums => {
+    if (nums.length <= 1) return nums;
+    
+    const pivot = nums[nums.length-1];
+    const left = [];
+    const right = [];
+    
+    for (let i = 0; i < nums.length-1; i++) {
+      if (nums[i] < pivot) {
+        left.push(nums[i]);
+      }
+      else {
+        right.push(nums[i]);
+      }
+    }
+    return [...quickSort(left), pivot, ...quickSort(right)];
+  };
