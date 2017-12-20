a38bce9c70d4ece685f2eaa12cab1fa7752bc651
Edwin Mendez <mendezwin@gmail.com>
2017-12-20 12:06:51 -0500

    Merge sort updated

--- algorithms/4 - Merge Sort.js
+++ algorithms/4 - Merge Sort.js
@@ -2,6 +2,8 @@
   Write a function that performs mergesort
   Name the function mergeSort
   It will take in a array of numbers and return a sorted array numbers
+
+  The basic gist of merge sort is that you're going to take your big list, and first divide down in two half size lists and recursively call merge sort on those smaller list, which in turn will do the same. The base case is when you have a list of one, at which point you will return that sorted list of one. On the way up the recursive calls, you will merge those sorted lists together (preferably by another merge function you'll write) that walks through both lists simultaneously and inserts the smaller value first, effectively creating a bigger sorted list.
 */
 
 const mergeSort = nums => {
